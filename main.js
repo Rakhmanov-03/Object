ea8a71fe2709a25e979dcf9c6fe330f6ec406729
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2024-01-14 16:47:28 +0500

    5-2.dars_vazifa

---
+++ main.js
@@ -0,0 +1,84 @@
+// 1-masala
+
+// let arr1=+prompt("1-qiymat")
+// let arr2=+prompt("2-qiymat")
+// console.log([arr2, arr1]);
+
+// 2-masala
+
+// function massivOrtasi(){
+//     let arr=+prompt("Massivni kiriting");
+//     let counter = 0;
+//     for(let i=0; i<arr.length; i++){
+//         counter += arr[i];
+//     }
+//         let ortachaQiymat = counter / arr.length;
+//         console.log(ortachaQiymat);
+// }
+// massivOrtasi();
+
+// 3-masala
+
+// let massivStr = prompt("Massivni kiriting");
+// let massiv = massivStr.split(",").map(Number); 
+// let birinchiMassiv = massiv.slice(0, 2);
+// let ikkinchiMassiv = massiv.slice(2);
+// console.log(massiv);
+// console.log([birinchiMassiv, ...ikkinchiMassiv]);
+
+// 4-masala
+
+// let myObject = { person: { name: "John", age: 30 } };
+
+// let {
+//   person: { name, age }
+// } = myObject;
+
+// console.log("Ismi:" + name,"Yoshi:" + age);  
+
+// 5-masala
+
+// let myObject = { person: { name: "John", age: 30 } };
+
+// let {
+//   person: { name = "Alex", age = 25, gender = "Nomalum" } = {}
+// } = myObject;
+
+// console.log("Ismi:" + name,"Yoshi:" + age,"Jinsi:" + gender);
+
+// 6-masala
+
+// const arrayObject = [
+//     { person: { name: "Rustam", age: 30, gender: "Erkak" } },
+//     { person: { name: "Nozima", age: 25, gender: "Ayol" } },
+//     { person: { name: "Eldor", age: 40, gender: "Erkak" } }
+//   ];
+//   const Natija = arrayObject.map(({ person: { name, age, gender } }) => ({ name, age, gender }));
+  
+//   console.log(Natija);
+
+// 7-masala
+// function kordinatalar(x, y) {
+//     console.log("");
+//     console.log("X:", x);
+//     console.log("Y:", y);
+//   }
+//   function obyect({ x, y }) {
+//     kordinatalar(x, y);
+//   }
+//   const obyectKordinatasi = { x: 10, y: 20 };
+//   obyect(obyectKordinatasi);
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
